b0f0bf7bd68f6bcbb617509bc77a50abd8c83a5e
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-03-24 20:32:16 -0400

    first commit

---
+++ lib/ColumbusBeer.rb
@@ -0,0 +1,6 @@
+require "ColumbusBeer/version"
+
+module ColumbusBeer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ColumbusBeer/version.rb
@@ -0,0 +1,3 @@
+module ColumbusBeer
+  VERSION = "0.1.0"
+end
